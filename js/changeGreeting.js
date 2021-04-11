8f8c7046456e4083b6ba6d2fd050cde5f711433e
dohee93 <rlaehgml03@gmail.com>
2021-04-12 01:00:09 +0900

    Add index, hello, talk section

---
+++ js/changeGreeting.js
@@ -0,0 +1,15 @@
+var greetings = ['Hello.', 'Hola.','Buenas.', 'Ola.', 'Ei.', "E'yo.", 'Ello', 'Aye.'];
+var greetNum = greetings.length;
+var currentIdx = 0;
+var heroTitle = document.getElementById('hero-title');
+
+function changeGreeting() {
+   if(currentIdx==greetNum) currentIdx = 0;
+   heroTitle.innerHTML = greetings[currentIdx];
+   currentIdx++;
+}
+
+function init() {
+   setInterval(changeGreeting, 1000);
+}
+init();
